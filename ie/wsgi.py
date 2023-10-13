a13c1a2b03aea12c5ee560a6d8fbddfb6805cf15
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-10-13 20:50:07 -0300

    fix

--- ie/wsgi.py
+++ ie/wsgi.py
@@ -8,15 +8,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os, sys
-
+import my_logging as mylog
 #path a donde esta el manage.py de nuestro proyecto Django
 sys.path.append('/home/lbisaro/dj_app/ie/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ie.settings')
 
-cmd = 'source /home/lbisaro/dj_app/ie/venv/bin/activate'
-os.system(cmd)
-
+mylog.info(sys.version_info)
 from django.core.wsgi import get_wsgi_application
 
 
