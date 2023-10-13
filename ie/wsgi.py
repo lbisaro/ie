2d9a559a285e12c519f8ede90ab22dd9e735edda
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-10-13 09:52:30 -0300

    Fixes

--- ie/wsgi.py
+++ ie/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+#path a donde esta el manage.py de nuestro proyecto Django
+sys.path.append('/home/lbisaro/dj_app/ie/')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ie.settings')
+
+activate_this = '/home/lbisaro/dj_app/ie/venv/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ie.settings')
+
 
 application = get_wsgi_application()
