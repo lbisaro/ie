2b43ed019cd1bd03e336d2df9d7fc3cad8ec5f42
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-10-13 20:42:37 -0300

    Update wsgi.py

--- ie/wsgi.py
+++ ie/wsgi.py
@@ -14,9 +14,8 @@ sys.path.append('/home/lbisaro/dj_app/ie/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ie.settings')
 
-activate_this = '/home/lbisaro/dj_app/ie/venv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this)) 
+cmd = 'source /home/lbisaro/dj_app/ie/venv/bin/activate'
+os.system(cmd)
 
 from django.core.wsgi import get_wsgi_application
 
