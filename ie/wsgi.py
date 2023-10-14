cc464acb52e3e18ea4f57a518f23f3135b17cd01
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-10-14 09:58:58 -0300

    fix venv

--- ie/wsgi.py
+++ ie/wsgi.py
@@ -8,15 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os, sys
-import my_logging as mylog
 #path a donde esta el manage.py de nuestro proyecto Django
 sys.path.append('/home/lbisaro/dj_app/ie/')
+sys.path.append('/home/lbisaro/dj_app/ie/venv/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ie.settings')
 
-mylog.info(sys.version_info)
 from django.core.wsgi import get_wsgi_application
-
-
 
 application = get_wsgi_application()
