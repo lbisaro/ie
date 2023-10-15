f1f4d005ceb9a0333ca0f1c7221c3b876b6418e9
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-10-15 13:30:48 -0300

    fix

--- app_log.py
+++ app_log.py
@@ -3,13 +3,14 @@ import logging
 from datetime import datetime
 
 class app_log:
-    filename = 'bot'
+    filename = 'log/bot'
     date_format = '%Y-%m-%d %H:%M:%S'
 
     def write(self,type,msg):
         dt = datetime.now()
         line = dt.strftime(self.date_format)+' - '+type+' - '+msg
-        with open(self.filename+'.log', 'a') as file:
+        filename_add = dt.strftime('%Y%m%d%H')
+        with open(self.filename+'_'+filename_add+'.log', 'a') as file:
             file.write(line + '\n')
 
     def info(self,msg):
